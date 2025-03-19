619c7d590ed18c75d5cfe727b52265912e6b495f
Dan Matthews <dan@danmatthews.me>
2025-03-19 15:15:12 +0000

    feat: add debug logging to OG generation

--- app/Console/Commands/GenerateOGImages.php
+++ app/Console/Commands/GenerateOGImages.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Models\BlogPost;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Colors\Rgb\Color;
 use SimonHamp\TheOg\Background;
 use SimonHamp\TheOg\Image;
@@ -35,28 +36,34 @@ class GenerateOGImages extends Command
 
         File::ensureDirectoryExists(storage_path('app/public/opengraph'));
         BlogPost::all()->each(function ($post) {
-            (new Image())
-                ->accentColor('#cc0000')
-                ->theme(
-                    new Theme(
-                        accentColor: '#14b8a6',
-                        baseFont: Inter::bold(),
-                        baseColor: '#ffffff',
-                        backgroundColor: '#272727',
-                        background: new \SimonHamp\TheOg\Theme\Background(public_path('images/og-background.png')),
-                        callToActionBackgroundColor: '#153B50',
-                        callToActionColor: '#ECEBE4',
-                        descriptionColor: '#cccccc',
-                        descriptionFont: Inter::light(),
-                        titleFont: Inter::black(),
-                    )
-                )
-                ->border(color: Color::create('#272727'), width: 0)
-                ->url(route('posts.show', ['blogPost' => $post]))
-                ->title($post->title)
-                ->description($post->excerpt)
-                ->save(storage_path('app/public/opengraph') . "/{$post->id}.png");
-        });
 
+            $this->info("Generating OG image for {$post->id}");
+            try {
+                (new Image())
+                    ->accentColor('#cc0000')
+                    ->theme(
+                        new Theme(
+                            accentColor: '#14b8a6',
+                            baseFont: Inter::bold(),
+                            baseColor: '#ffffff',
+                            backgroundColor: '#272727',
+                            background: new \SimonHamp\TheOg\Theme\Background(public_path('images/og-background.png')),
+                            callToActionBackgroundColor: '#153B50',
+                            callToActionColor: '#ECEBE4',
+                            descriptionColor: '#cccccc',
+                            descriptionFont: Inter::light(),
+                            titleFont: Inter::black(),
+                        )
+                    )
+                    ->border(color: Color::create('#272727'), width: 0)
+                    ->url(route('posts.show', ['blogPost' => $post]))
+                    ->title($post->title)
+                    ->description($post->excerpt)
+                    ->save(storage_path('app/public/opengraph') . "/{$post->id}.png");
+                $this->info("Generated at " . Storage::disk('public')->url('opengraph/' . $post->id . '.png'));
+            } catch (\Throwable $e) {
+                $this->error($e->getMessage());
+            }
+        });
     }
 }
