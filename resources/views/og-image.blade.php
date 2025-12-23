e35a6a82421150868ec31584b904e3f978689410
Dan Matthews <dan@danmatthews.me>
2025-12-23 16:32:37 +0000

    Update OG image style.

--- resources/views/og-image.blade.php
+++ resources/views/og-image.blade.php
@@ -7,17 +7,25 @@
     <script src="https://cdn.tailwindcss.com"></script>
     @vite(['resources/css/app.css'])
 </head>
-<body class="bg-red-400">
-<div class="w-[1200px] h-[630px] overflow-hidden">
+<body class="font-serif">
+<div class="w-[1200px] h-[630px] overflow-hidden og-image">
     <div
-        class="relative p-[60px] pr-[365px]  bg-zinc-900 w-full h-full">
+        class="relative p-[60px] pr-[365px]  bg-white w-full h-full">
 
-        <div>
-            <h1 class="text-5xl font-bold text-white mb-6 leading">{{ $title }}</h1>
-            @if(isset($excerpt) && $excerpt)
-                <p class="text-2xl text-gray-300 mb-8">{{ $excerpt }}</p>
-            @endif
+        <div class="h-full flex flex-col items-between justify-between">
+            <div>
+                <h1 class="text-5xl mb-6 leading font-serif">{{ $title }}</h1>
+                @if(isset($excerpt) && $excerpt)
+                    <p class="text-2xl text-gray-500 mb-8">{{ $excerpt }}</p>
+                @endif
+            </div>
+            @isset($url)
+            <div class="underline decoration-gray-300 underline-offset-4 text-subtle">
+                {{ $url }}
+            </div>
+            @endisset
         </div>
+
 
         <img
             src="{{ asset('icons/favicon.svg') }}"
--- routes/web.php
+++ routes/web.php
@@ -8,137 +8,152 @@ use Inertia\Inertia;
 
 Route::feeds();
 
-Route::get('/', function () {
-    $posts = fn() => BlogPost::orderBy('date', 'DESC')
-        ->limit(3)
-        ->get()
-        ->map(fn(BlogPost $post) => [
-            'id' => $post->id,
-            'title' => $post->title,
-            'slug' => $post->slug,
-            'excerpt' => $post->excerpt,
-            'date' => [
-                'iso' => $post->date->format('c'),
-                'formatted' => $post->date->format('jS F Y'),
+Route::get("/", function () {
+    $posts = fn() => BlogPost::orderBy("date", "DESC")->limit(3)->get()->map(
+        fn(BlogPost $post) => [
+            "id" => $post->id,
+            "title" => $post->title,
+            "slug" => $post->slug,
+            "excerpt" => $post->excerpt,
+            "date" => [
+                "iso" => $post->date->format("c"),
+                "formatted" => $post->date->format("jS F Y"),
             ],
-            'url' => route('posts.show', ['blog_post' => $post]),
-        ]);
+            "url" => route("posts.show", ["blog_post" => $post]),
+        ],
+    );
 
-    $page = request('page', 1);
+    $page = request("page", 1);
 
-    return Inertia::render('Index', [
-        'posts' => $posts,
-        'pageTitle' => "Welcome",
-        'intro' => config('site.posts'),
+    return Inertia::render("Index", [
+        "posts" => $posts,
+        "pageTitle" => "Welcome",
+        "intro" => config("site.posts"),
     ]);
+})->name("home");
 
-})->name('home');
-
-Route::get('posts', function () {
-    $posts = fn() => BlogPost::orderBy('date', 'DESC')
+Route::get("posts", function () {
+    $posts = fn() => BlogPost::orderBy("date", "DESC")
         ->paginate(50)
-        ->through(fn(BlogPost $post) => [
-            'id' => $post->id,
-            'title' => $post->title,
-            'slug' => $post->slug,
-            'excerpt' => $post->excerpt,
-            'date' => [
-                'iso' => $post->date->format('c'),
-                'formatted' => $post->date->format('jS F Y'),
+        ->through(
+            fn(BlogPost $post) => [
+                "id" => $post->id,
+                "title" => $post->title,
+                "slug" => $post->slug,
+                "excerpt" => $post->excerpt,
+                "date" => [
+                    "iso" => $post->date->format("c"),
+                    "formatted" => $post->date->format("jS F Y"),
+                ],
+                "url" => route("posts.show", ["blog_post" => $post]),
             ],
-            'url' => route('posts.show', ['blog_post' => $post]),
-        ])
+        )
         ->withQueryString();
 
-    $page = request('page', 1);
+    $page = request("page", 1);
 
-    return Inertia::render('Posts/Index', [
-        'posts' => $posts,
-        'pageTitle' => $page == 1 ? 'Posts' : "Posts (Page {$page})",
-        'intro' => config('site.posts'),
+    return Inertia::render("Posts/Index", [
+        "posts" => $posts,
+        "pageTitle" => $page == 1 ? "Posts" : "Posts (Page {$page})",
+        "intro" => config("site.posts"),
     ]);
-})->name('posts.index');
+})->name("posts.index");
 
-Route::get('posts/{blog_post}', function (BlogPost $blogPost) {
-    $blogPost->load('tags');
+Route::get("posts/{blog_post}", function (BlogPost $blogPost) {
+    $blogPost->load("tags");
 
-    return Inertia::render('Posts/Show', [
-        'post' => [
-            'id' => $blogPost->id,
-            'title' => $blogPost->title,
-            'excerpt' => $blogPost->excerpt,
-            'content' => $blogPost->content,
-            'url' => url()->current(),
-            'og_image' => asset('storage/opengraph/' . $blogPost->id . '.png'),
-            'date' => [
-                'iso' => $blogPost->date->format('c'),
-                'formatted' => $blogPost->date->format('jS F Y'),
+    return Inertia::render("Posts/Show", [
+        "post" => [
+            "id" => $blogPost->id,
+            "title" => $blogPost->title,
+            "excerpt" => $blogPost->excerpt,
+            "content" => $blogPost->content,
+            "url" => url()->current(),
+            "og_image" => asset("storage/opengraph/" . $blogPost->id . ".png"),
+            "date" => [
+                "iso" => $blogPost->date->format("c"),
+                "formatted" => $blogPost->date->format("jS F Y"),
             ],
-            'monthsAgo' => now()->diffInMonths($blogPost->date),
-            'tags' => $blogPost->tags?->pluck('tag') ?? [],
+            "monthsAgo" => now()->diffInMonths($blogPost->date),
+            "tags" => $blogPost->tags?->pluck("tag") ?? [],
         ],
     ]);
-})->name('posts.show');
+})->name("posts.show");
 
-Route::get('/tags', function () {
-    $tags = PostTag::get()->groupBy('tag')->map(function ($posts, $tag) {
-        return [
-            'tag' => $tag,
-            'count' => $posts->count(),
-        ];
-    })->values();
-
-    return Inertia::render('Tags/Index', [
-        'tags' => $tags,
+Route::get("posts/{blog_post}/og", function (BlogPost $blogPost) {
+    return view("og-image", [
+        "title" => $blogPost->title,
+        "excerpt" => $blogPost->excerpt,
+        "url" => route("posts.show", $blogPost),
     ]);
-})->name('tags.index');
+});
 
-Route::get('/tags/{id}', function (string $id) {
-    $posts = BlogPost::orderBy('date', 'DESC')
-        ->whereHas('tags', fn($query) => $query->where('tag', $id))
+Route::get("/tags", function () {
+    $tags = PostTag::get()
+        ->groupBy("tag")
+        ->map(function ($posts, $tag) {
+            return [
+                "tag" => $tag,
+                "count" => $posts->count(),
+            ];
+        })
+        ->values();
+
+    return Inertia::render("Tags/Index", [
+        "tags" => $tags,
+    ]);
+})->name("tags.index");
+
+Route::get("/tags/{id}", function (string $id) {
+    $posts = BlogPost::orderBy("date", "DESC")
+        ->whereHas("tags", fn($query) => $query->where("tag", $id))
         ->paginate(10)
-        ->through(fn(BlogPost $post) => [
-            'id' => $post->id,
-            'title' => $post->title,
-            'slug' => $post->slug,
-            'excerpt' => $post->excerpt,
-            'date' => [
-                'iso' => $post->date->format('c'),
-                'formatted' => $post->date->format('jS F Y'),
+        ->through(
+            fn(BlogPost $post) => [
+                "id" => $post->id,
+                "title" => $post->title,
+                "slug" => $post->slug,
+                "excerpt" => $post->excerpt,
+                "date" => [
+                    "iso" => $post->date->format("c"),
+                    "formatted" => $post->date->format("jS F Y"),
+                ],
+                "url" => route("posts.show", ["blog_post" => $post]),
             ],
-            'url' => route('posts.show', ['blog_post' => $post]),
-        ])
+        )
         ->withQueryString();
 
-    return Inertia::render('Tags/Show', [
-        'tag' => $id,
-        'posts' => $posts,
+    return Inertia::render("Tags/Show", [
+        "tag" => $id,
+        "posts" => $posts,
     ]);
-})->name('tags.show');
+})->name("tags.show");
 
-Route::get('/about', function () {
-    $resume = ResumeEntry::all()->map(fn(ResumeEntry $entry) => [
-        'companyName' => $entry->companyName,
-        'jobTitle' => $entry->jobTitle,
-        'start' => $entry->start,
-        'end' => $entry->end,
-        'url' => $entry->url,
+Route::get("/about", function () {
+    $resume = ResumeEntry::all()->map(
+        fn(ResumeEntry $entry) => [
+            "companyName" => $entry->companyName,
+            "jobTitle" => $entry->jobTitle,
+            "start" => $entry->start,
+            "end" => $entry->end,
+            "url" => $entry->url,
+        ],
+    );
+
+    return Inertia::render("About/Index", [
+        "about" => markdown(config("site.about")),
+        "resume" => $resume,
     ]);
+})->name("about");
 
-    return Inertia::render('About/Index', [
-        'about' => markdown(config('site.about')),
-        'resume' => $resume,
+Route::get("/work", function () {
+    $projects = collect(config("site.projects"))->map(
+        fn($project) => $project->toArray(),
+    );
+
+    return Inertia::render("Work/Index", [
+        "projects" => $projects,
     ]);
-})->name('about');
+})->name("work");
 
-Route::get('/work', function () {
-    $projects = collect(config('site.projects'))
-        ->map(fn($project) => $project->toArray());
-
-    return Inertia::render('Work/Index', [
-        'projects' => $projects,
-    ]);
-})->name('work');
-
-Route::get('/uses', fn() => Inertia::render('Uses/Index'))->name('uses');
-
+Route::get("/uses", fn() => Inertia::render("Uses/Index"))->name("uses");
