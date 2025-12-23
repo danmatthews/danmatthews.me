d0a26b6c02144d9ea05081b12a6b997eeda8b734
Dan Matthews <dan@danmatthews.me>
2025-12-23 16:19:38 +0000

    Add watch plugin + update css.

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,20 @@
-import {defineConfig} from 'vite';
-import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
+import vue from "@vitejs/plugin-vue";
+import { watch } from "vite-plugin-watch";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
         vue(),
         tailwindcss(),
+        watch({
+            pattern: "content/**/*.md",
+            command: "php artisan build:posts",
+        }),
     ],
 });
