44cd7f707e3204ed873e6374ef5b4fcbd5f20431
Dan Matthews <dan@danmatthews.me>
2025-12-23 16:36:17 +0000

    Move postmeta to the server-rendered part.

---
+++ resources/views/post-meta.blade.php
@@ -0,0 +1,11 @@
+  <meta name="description" content="{{ $postMeta['excerpt'] }}">
+  <meta property="og:site_name" content="{{ $postMeta['site_name'] }}">
+  <meta property="og:type" content="article">
+  <meta property="og:title" content="{{ $postMeta['title'] }}">
+  <meta property="og:description" content="{{ $postMeta['excerpt'] }}">
+  <meta property="og:image" content="{{ $postMeta['og_image'] }}">
+  <meta name="twitter:title" content=" {{ $postMeta['title'] }}">
+  <meta name="twitter:description" content="{{ $postMeta['excerpt'] }}">
+  <meta name="twitter:image" content="{{ $postMeta['og_image'] }}">
+  <link rel="canonical" href="{{ $postMeta['url'] }}">
+  <component is="script" type="application/ld+json">{{ $postMeta['structured_data'] }}</component>
